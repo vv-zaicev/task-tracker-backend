0019cab10d6665200e4b7e37080270331eecc89e
Vadim Zaicev <zaicevvadim9@gmail.com>
2025-05-23 21:20:50 +0600

    подключен liquibase для тестов

--- src/test/java/com/zaicev/task_tracker_backend/TestcontainersConfiguration.java
+++ src/test/java/com/zaicev/task_tracker_backend/TestcontainersConfiguration.java
@@ -3,10 +3,12 @@ package com.zaicev.task_tracker_backend;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
+@TestPropertySource(properties = {"spring.config.location=classpath:application-test.properties"})
 class TestcontainersConfiguration {
 
 	@Bean
