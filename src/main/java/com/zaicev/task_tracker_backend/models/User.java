a66c0ebba0523008f17e82820ccfd4162cf3214b
Vadim Zaicev <zaicevvadim9@gmail.com>
2025-05-30 17:35:04 +0600

    Пользователь теперь реализует UserDetails

--- src/main/java/com/zaicev/task_tracker_backend/models/User.java
+++ src/main/java/com/zaicev/task_tracker_backend/models/User.java
@@ -1,8 +1,14 @@
 package com.zaicev.task_tracker_backend.models;
 
+import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
@@ -25,20 +31,60 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Column(unique = true, nullable = false)
 	private String email;
 
+	@Column(unique = true, nullable = false)
+	private String username;
+
+	@Column(nullable = false)
 	private String password;
+
+	@Column(name = "verification_code")
+	private String verificationCode;
+
+	@Column(name = "verification_expiration")
+	private LocalDateTime verificationCodeExpiresAt;
+
+	private boolean enabled;
 
 	@Builder.Default
 	@ElementCollection(fetch = FetchType.EAGER)
-	@CollectionTable(name = "user_roles", joinColumns = {@JoinColumn(name = "user_id")})
+	@CollectionTable(name = "user_roles", joinColumns = { @JoinColumn(name = "user_id") })
 	@Column(name = "role")
 	private Set<String> roles = new HashSet<>();
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles.stream()
+				.map(x -> new SimpleGrantedAuthority(x))
+				.toList();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return enabled;
+	}
 
 	@Override
 	public boolean equals(Object obj) {
@@ -54,6 +100,5 @@ public class User {
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-	
-	
+
 }
--- src/main/java/com/zaicev/task_tracker_backend/security/services/TokenAuthenticationUserDetailsService.java
+++ src/main/java/com/zaicev/task_tracker_backend/security/services/TokenAuthenticationUserDetailsService.java
@@ -7,19 +7,19 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 
+import com.zaicev.task_tracker_backend.models.User;
 import com.zaicev.task_tracker_backend.security.models.Token;
-import com.zaicev.task_tracker_backend.security.models.UserDetailsImpl;
 
-public class TokenAuthenticationUserDetailsService implements AuthenticationUserDetailsService<PreAuthenticatedAuthenticationToken>{
+public class TokenAuthenticationUserDetailsService implements AuthenticationUserDetailsService<PreAuthenticatedAuthenticationToken> {
 
 	@Override
 	public UserDetails loadUserDetails(PreAuthenticatedAuthenticationToken authenticationToken) throws UsernameNotFoundException {
-		if(authenticationToken.getPrincipal() instanceof Token token) {
-			return new UserDetailsImpl(token.subject(), "nopassword", new HashSet<String>(token.authorites()));
+		if (authenticationToken.getPrincipal() instanceof Token token) {
+			User user = new User().builder().email(token.subject()).password("nopassword").roles(new HashSet<String>(token.authorites())).build();
+			return user;
 		}
-		
-		
+
 		throw new UsernameNotFoundException("Principal must be of type Token");
 	}
-	
+
 }
