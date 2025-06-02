31e9a877d2cb103a292bd710bb7df552631801e7
Vadim Zaicev <zaicevvadim9@gmail.com>
2025-06-02 21:26:16 +0600

    добавлен контроллер пользователей

---
+++ src/main/java/com/zaicev/task_tracker_backend/controllers/UsersController.java
@@ -0,0 +1,23 @@
+package com.zaicev.task_tracker_backend.controllers;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zaicev.task_tracker_backend.converters.DefaultUserDTOConverter;
+import com.zaicev.task_tracker_backend.converters.UserDTOConverter;
+import com.zaicev.task_tracker_backend.dto.UserResponseDTO;
+import com.zaicev.task_tracker_backend.models.User;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+	
+	private final UserDTOConverter userDTOConverter = new DefaultUserDTOConverter();
+	
+	@GetMapping("/me")
+	public UserResponseDTO getMe(@AuthenticationPrincipal User user) {
+		return userDTOConverter.toDTO(user);
+	}
+}
