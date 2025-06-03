2844cc3a1deda75d649191a4746f9b781a50d212
Vadim Zaicev <zaicevvadim9@gmail.com>
2025-06-03 23:02:44 +0600

    сконфигурирован spring security

--- src/main/java/com/zaicev/task_tracker_backend/config/SecurityConfig.java
+++ src/main/java/com/zaicev/task_tracker_backend/config/SecurityConfig.java
@@ -1,12 +1,107 @@
 package com.zaicev.task_tracker_backend.config;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.nimbusds.jose.crypto.DirectDecrypter;
+import com.nimbusds.jose.crypto.DirectEncrypter;
+import com.nimbusds.jose.jwk.OctetSequenceKey;
+import com.zaicev.task_tracker_backend.authentication.JsonAuthenticationConfigurer;
+import com.zaicev.task_tracker_backend.authentication.cookie.TokenCookieAuthenticationConfigurer;
+import com.zaicev.task_tracker_backend.authentication.cookie.TokenCookieJweStringDeserializer;
+import com.zaicev.task_tracker_backend.authentication.cookie.TokenCookieJweStringSerializer;
+import com.zaicev.task_tracker_backend.authentication.cookie.TokenCookieSessionAuthenticationStrategy;
 
 @Configuration
 @EnableWebSecurity
 @ComponentScan("com.zaicev.task_tracker_backend.security")
 public class SecurityConfig {
 
+	@Value("${FRONTEND_URL}")
+	private String frontendURL;
+
+	@Bean
+	TokenCookieAuthenticationConfigurer tokenCookieAuthenticationConfigurer(@Value("${jwt.cookie-token-key}") String cookieTokenKey)
+			throws Exception {
+		return new TokenCookieAuthenticationConfigurer()
+				.tokenCookieStringDeserializer(new TokenCookieJweStringDeserializer(new DirectDecrypter(OctetSequenceKey.parse(cookieTokenKey))));
+	}
+
+	@Bean
+	JsonAuthenticationConfigurer jsonAuthenticationConfigurer(TokenCookieSessionAuthenticationStrategy tokenCookieSessionAuthenticationStrategy) {
+		return new JsonAuthenticationConfigurer("/auth/sign-in", tokenCookieSessionAuthenticationStrategy);
+	}
+
+	@Bean
+	TokenCookieJweStringSerializer tokenCookieJweStringSerializer(@Value("${jwt.cookie-token-key}") String cookieTokenKey) throws Exception {
+		return new TokenCookieJweStringSerializer(new DirectEncrypter(OctetSequenceKey.parse(cookieTokenKey)));
+	}
+
+	@Bean
+	TokenCookieSessionAuthenticationStrategy tokenCookieSessionAuthenticationStrategy(TokenCookieJweStringSerializer tokenCookieJweStringSerializer) {
+		TokenCookieSessionAuthenticationStrategy tokenCookieSessionAuthenticationStrategy = new TokenCookieSessionAuthenticationStrategy();
+		tokenCookieSessionAuthenticationStrategy.setTokenStringSerializer(tokenCookieJweStringSerializer);
+		return tokenCookieSessionAuthenticationStrategy;
+	}
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, TokenCookieAuthenticationConfigurer tokenCookieAuthenticationConfigurer,
+			JsonAuthenticationConfigurer jsonAuthenticationConfigurer)
+			throws Exception {
+		httpSecurity
+				.httpBasic(Customizer.withDefaults())
+				.authorizeHttpRequests(authorizeHttpRequest -> authorizeHttpRequest
+						.requestMatchers("/auth/**", "/error")
+						.permitAll()
+						.anyRequest()
+						.authenticated())
+				.sessionManagement(sessionManagement -> sessionManagement
+						.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.logout(logout -> logout
+						.logoutUrl("/auth/logout"))
+				.csrf(csrf -> csrf
+						.csrfTokenRepository(new CookieCsrfTokenRepository())
+						.csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler())
+						.sessionAuthenticationStrategy(((authentication, request, response) -> {
+						})));
+
+		httpSecurity.with(tokenCookieAuthenticationConfigurer, Customizer.withDefaults());
+		httpSecurity.with(jsonAuthenticationConfigurer, Customizer.withDefaults());
+
+		return httpSecurity.build();
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList(frontendURL));
+		configuration.setAllowedMethods(Arrays.asList("*"));
+		configuration.setAllowedHeaders(Arrays.asList("*"));
+		configuration.setAllowCredentials(true);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
 }
