8be0431e5ae614f9ee704f11b7a6bb03fbbac1f4
Vadim Zaicev <zaicevvadim9@gmail.com>
2025-06-08 14:32:49 +0600

    дефолтный реализации интерфейсов конвертеров создаются как бины

--- src/main/java/com/zaicev/task_tracker_backend/TaskTrackerBackendApplication.java
+++ src/main/java/com/zaicev/task_tracker_backend/TaskTrackerBackendApplication.java
@@ -2,12 +2,26 @@ package com.zaicev.task_tracker_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.zaicev.task_tracker_backend.converters.TaskDTOConverter;
+import com.zaicev.task_tracker_backend.converters.UserDTOConverter;
 
 @SpringBootApplication
 public class TaskTrackerBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskTrackerBackendApplication.class, args);
+	}
+
+	@Bean
+	TaskDTOConverter defaultTaskDTOConverter() {
+		return new TaskDTOConverter() {};
+	}
+	
+	@Bean
+	UserDTOConverter defaultUserDTOConverter() {
+		return new UserDTOConverter() {};
 	}
 
 }
