ff2d20c9642b95cd90dd40ada670124b9f1b5723
Vadim Zaicev <zaicevvadim9@gmail.com>
2025-07-23 22:47:10 +0600

    Добавлен эндпоинт для получения всех пользователей

--- src/main/java/com/zaicev/task_tracker_backend/controllers/UsersController.java
+++ src/main/java/com/zaicev/task_tracker_backend/controllers/UsersController.java
@@ -1,5 +1,7 @@
 package com.zaicev.task_tracker_backend.controllers;
 
+import java.util.List;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,19 +10,27 @@ import org.springframework.web.bind.annotation.RestController;
 import com.zaicev.task_tracker_backend.converters.UserDTOConverter;
 import com.zaicev.task_tracker_backend.dto.UserResponseDTO;
 import com.zaicev.task_tracker_backend.models.User;
+import com.zaicev.task_tracker_backend.services.UserService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/users")
 public class UsersController {
 
 	private final UserDTOConverter userDTOConverter;
 
-	public UsersController(UserDTOConverter userDTOConverter) {
-		this.userDTOConverter = userDTOConverter;
+	private final UserService userService;
+
+	@GetMapping()
+	public List<UserResponseDTO> getUsers() {
+		return userService.getUsers();
 	}
 
 	@GetMapping("/me")
 	public UserResponseDTO getMe(@AuthenticationPrincipal User user) {
 		return userDTOConverter.toDTO(user);
 	}
+
 }
--- src/test/java/com/zaicev/task_tracker_backend/controllers/UserControllerTest.java
+++ src/test/java/com/zaicev/task_tracker_backend/controllers/UserControllerTest.java
@@ -1,11 +1,14 @@
 package com.zaicev.task_tracker_backend.controllers;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.Collections;
+import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,32 +22,57 @@ import org.springframework.test.web.servlet.MockMvc;
 import com.zaicev.task_tracker_backend.converters.UserDTOConverter;
 import com.zaicev.task_tracker_backend.dto.UserResponseDTO;
 import com.zaicev.task_tracker_backend.models.User;
-import com.zaicev.task_tracker_backend.repository.UserRepository;
+import com.zaicev.task_tracker_backend.services.UserService;
 
 @WebMvcTest(UsersController.class)
 public class UserControllerTest {
+
 	@Autowired
 	private MockMvc mockMvc;
 
 	@MockitoBean
 	private UserDTOConverter userDTOConverter;
-	
-	@MockitoBean
-	private UserRepository userRepository;
 
-	@Test
-	void getMe_ShouldReturnUserData() throws Exception {
-		User testUser = new User();
+	@MockitoBean
+	private UserService userService;
+
+	private User testUser;
+
+	private UserResponseDTO userResponse = new UserResponseDTO(1L, "testUser", "test@example.com");
+
+	private Authentication authentication;
+
+	@BeforeEach
+	void setUp() {
+		testUser = new User();
 		testUser.setUsername("testUser");
 		testUser.setEmail("test@example.com");
 
-		Authentication authentication = new UsernamePasswordAuthenticationToken(
-				testUser,
-				null,
-				Collections.emptyList());
+		authentication = new UsernamePasswordAuthenticationToken(testUser, null, Collections.emptyList());
+	}
 
+	@Test
+	void getUsers_ShouldReturnUserList() throws Exception {
+		when(userService.getUsers()).thenReturn(List.of(userResponse));
+
+		mockMvc.perform(get("/users").with(SecurityMockMvcRequestPostProcessors.authentication(authentication)))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.length()").value(1))
+				.andExpect(jsonPath("$[0].id").value(userResponse.id()))
+				.andExpect(jsonPath("$[0].username").value(userResponse.username()))
+				.andExpect(jsonPath("$[0].email").value(userResponse.email()));
+	}
+
+	@Test
+	void getUsers_Unauthorized_ShouldReturn401() throws Exception {
+		mockMvc.perform(get("/users"))
+				.andExpect(status().isUnauthorized());
+	}
+
+	@Test
+	void getMe_ShouldReturnUserData() throws Exception {
 		Mockito.when(userDTOConverter.toDTO(testUser))
-				.thenReturn(new UserResponseDTO(1L, "testUser", "test@example.com"));
+				.thenReturn(userResponse);
 
 		mockMvc.perform(get("/users/me").with(SecurityMockMvcRequestPostProcessors.authentication(authentication)))
 				.andExpect(status().isOk())
